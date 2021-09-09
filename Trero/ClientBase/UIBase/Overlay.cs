9b140e83417b5c836173e3d7d56752ebff77e2ee
Laamy <mcpeconner0160@gmail.com>
2021-09-09 19:07:26 +1000

    Tonights finish up on the debug ui

--- Trero/ClientBase/UIBase/Overlay.cs
+++ Trero/ClientBase/UIBase/Overlay.cs
@@ -23,7 +23,7 @@ namespace Trero.ClientBase.UIBase
             MCM.RECT rect = MCM.getMinecraftRect();
 
             Location = new Point(rect.Left, rect.Top);
-            Size = new Size(rect.Right - rect.Left, rect.Bottom - rect.Top);
+            Size = new Size(rect.Right - rect.Left, rect.Bottom - rect.Top - 6);
 
             if (!MCM.isMinecraftFocused() && Opacity != 0)
                 Opacity = 0;
@@ -34,9 +34,10 @@ namespace Trero.ClientBase.UIBase
         private void Overlay_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.DrawString("Trero Template", new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, 36 + (0 * 14)));
-            e.Graphics.DrawString(Game.velocity.ToString(), new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, 36 + (1 * 14)));
-            e.Graphics.DrawString(Game.position.ToString(), new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, 36 + (2 * 14)));
-            e.Graphics.DrawString(Game.rotation.ToString(), new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, 36 + (3 * 14)));
+            e.Graphics.DrawString("ClientInstance: " + Game.clientInstance.ToString("X"), new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, Size.Height - 6 - (4 * 14)));
+            e.Graphics.DrawString("Pos: " + Game.position, new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, Size.Height - 6 - (3 * 14)));
+            e.Graphics.DrawString("Players: " + Game.getPlayers().Count, new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, Size.Height - 6 - (2 * 14)));
+            e.Graphics.DrawString("Entities: " + Game.getEntites().Count, new Font(FontFamily.GenericSansSerif, 12f), Brushes.Orange, new PointF(10, Size.Height - 6 - (1 * 14)));
         }
     }
 }
