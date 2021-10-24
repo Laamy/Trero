de32c657d0327bfd54788653e3632687a1998930
yeemi routh <shade4keal@gmail.com>
2021-10-24 22:18:30 +1100

    Mineplex step

---
+++ Trero/Modules/MineplexStep.cs
@@ -0,0 +1,45 @@
+﻿#region
+
+using System.Windows.Forms;
+using Trero.ClientBase;
+using Trero.ClientBase.KeyBase;
+using Trero.ClientBase.VersionBase;
+using Trero.Modules.vModuleExtra;
+
+#endregion
+
+namespace Trero.Modules
+{
+    internal class MineplexStep : Module
+    {
+        public MineplexStep() : base("MineplexStep", (char)0x07, "Player")
+        {
+            addBypass(new BypassBox(new string[] { "Default", "Fast", "Super Slow", "Slow" }));
+        } // Not defined
+
+        public override void OnTick()
+        {
+            float speed = 0.4f;
+
+            if (Game.touchingObject != 1) return;
+
+            switch (bypasses[0].curIndex)
+            {
+                case 0:
+                    speed = 0.4f;
+                    break;
+                case 1:
+                    speed = 0.6f;
+                    break;
+                case 2:
+                    speed = 0.1f;
+                    break;
+                case 3:
+                    speed = 0.2f;
+                    break;
+            }
+
+            MCM.writeFloat(Game.localPlayer + VersionClass.GetData("velocity") + 4, speed);
+        }
+    }
+}
