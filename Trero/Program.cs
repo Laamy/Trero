e5079c769bf2cfd361d5484899fbac9bfa536a22
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-10-31 14:03:39 +0100

    Update Program.cs

--- Trero/Program.cs
+++ Trero/Program.cs
@@ -139,6 +139,7 @@ namespace Trero
             Modules.Add(new ElytraFlight());
             Modules.Add(new Tower());
             Modules.Add(new Freelook());
+            Modules.Add(new SurroundTest());
 
             Console.WriteLine(@"Registered modules!");
 
