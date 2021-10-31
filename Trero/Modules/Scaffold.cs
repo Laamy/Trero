8595b760f07614abff04a3a7c63f1a3a81aad557
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-10-31 13:14:41 +0100

    Update Scaffold.cs

--- Trero/Modules/Scaffold.cs
+++ Trero/Modules/Scaffold.cs
@@ -8,11 +8,21 @@ namespace Trero.Modules
 {
     class Scaffold : Module
     {
+
+        float savedY = 0f;
         public Scaffold() : base("Scaffold", (char)0x07, "World") {
             addBypass(new BypassBox(new string[] { "Speed: Default", "Speed: 1", "Speed: 2" }));
             addBypass(new BypassBox(new string[] { "Place Mode: Auto ", "Place Mode: Manual" }));
-            //addBypass(new BypassBox(new string[] { "No Blocks Stop: true", "No Blocks Stop: false" })); HeldItemCount is broken
+            addBypass(new BypassBox(new string[] { "AntiFall: true", "AntiFall: false" }));
+            //addBypass(new BypassBox(new string[] { "No Blocks Stop: true", "No Blocks Stop: false" })); HeldItemCount is still broken
             //addBypass(new BypassBox(new string[] { "Mode: JumpBridge", "Mode: Beneath", "Mode: StairCase" }));
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            savedY = Game.position.y;
         }
 
         public override void OnDisable()
@@ -28,14 +38,35 @@ namespace Trero.Modules
 
             if (MCM.isMinecraftFocused())
             {
-                OverrideBase.lookingAtBlock = false;
                 Game.isLookingAtBlock = 0;
 
                 var scaffoldPos = Game.exactPos;
-                scaffoldPos.y -= 2;
-                Game.SelectedBlock = scaffoldPos;
-
+                scaffoldPos.y = (int)savedY - 1;
+                if (scaffoldPos.y != (int)savedY - 2)
+                {
+                    Game.SelectedBlock = scaffoldPos;
+                }
                 Game.SideSelect = 1;
+
+                switch (bypasses[2].curIndex)
+                {
+                    case 0:
+                        if(Game.position.y < (int)savedY)
+                        {
+                            if (Game.heldItemCount == 0)//HeldItemCount is still broken
+                            {
+                                Game.velocity = Base.Vec3();
+                            }
+                            else
+                            {
+                                Game.onGround = true;
+                                Game.velocity = Base.Vec3();
+                                Game.velocity = Base.Vec3(0, 0.3f);
+                            }
+                        }
+                        break;
+                }
+                
 
                 float _speed = 0;
 
